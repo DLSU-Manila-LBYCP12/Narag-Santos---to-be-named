11e4af94def70fe71c5719ad5525a9f610d15693
Gershom Narag <>
2017-08-14 07:26:39 +0800

    framework

---
+++ project/src/project/Project.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Gershom Narag/Cellix Santos
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+public class Project extends GraphicsProgram{
+
+    public void init(){
+        
+    }
+    
+    public void start_game(){
+        
+    }
+    
+    public void scene_Select(){
+        
+    }
+    
+    public void exit(){
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
