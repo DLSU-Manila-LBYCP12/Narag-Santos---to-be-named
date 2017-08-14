e7ab4e7e555073f60bcc5474ce93dec4390023e2
cellixsantos <cellix_santos@dlsu.edu.ph>
2017-08-14 18:59:32 +0800

    Update List.java

--- project/src/adt/interfaces/List.java
+++ project/src/adt/interfaces/List.java
@@ -16,17 +16,7 @@ public interface List<E> {
 
     public int size();
 
-    public void set(int index, E item) throws ListIndexOutOfBoundsException;
-
-    public void add(E e);
-
     public boolean contains(Object o);
-
-    public boolean containsAll(List<E> c);
-
-    public void addAll(List<E> c);
-
-    public void removeAll(List<E> c);
 
     public void clear();
 
@@ -34,5 +24,4 @@ public interface List<E> {
 
     public void sort();
 
-    public boolean intersection(List<E> list);
 }
