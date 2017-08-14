b6fedc7886447c610832f1f77761a1ebb55937d1
cellixsantos <cellix_santos@dlsu.edu.ph>
2017-08-14 18:50:33 +0800

    Create ListEmptyException.java

---
+++ project/src/adt/exceptions/ListEmptyException.java
@@ -0,0 +1,7 @@
+package adt.exceptions;
+
+public class ListEmptyException extends RuntimeException{
+    public ListEmptyException(String s){ 
+        super(s);
+    }//end constructor
+} //end ListException
