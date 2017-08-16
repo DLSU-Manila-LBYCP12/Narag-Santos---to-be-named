36b5a5ece5f47e2aeeab9f339a5d3eadc8b526d8
cellixsantos <cellix_santos@dlsu.edu.ph>
2017-08-17 03:31:09 +0800

    Update GameCanvas.java

--- project/src/game/main/GameCanvas.java
+++ project/src/game/main/GameCanvas.java
@@ -4,12 +4,22 @@ import game.interfaces.*;
 import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
+import game.chapters.Chapter1;
+import java.awt.Color;
 import java.awt.event.MouseListener;
 
-public class GameCanvas extends GCanvas implements GameConstants{
-        
+public class GameCanvas extends GCanvas implements GameConstants {
+    
+    private GImage textbox = new GImage("assets/ui/text_box.png");
+    private Chapter1 chapter1 = new Chapter1();
+    private GameUI ui = new GameUI();
+    
     public void init(){
         
-    }
+    }    
     
+    public void startchapter (){
+        chapter1.scene1(this);
+
+    }
 }
