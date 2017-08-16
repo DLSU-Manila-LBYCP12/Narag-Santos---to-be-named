682f7be8f32eed54a98dbb0d07d98a723da22014
cellixsantos <cellix_santos@dlsu.edu.ph>
2017-08-17 03:30:01 +0800

    Update GameUI.java

--- project/src/game/main/GameUI.java
+++ project/src/game/main/GameUI.java
@@ -12,30 +12,94 @@ import javax.imageio.ImageIO;
 
 public class GameUI extends GraphicsProgram{
         
-    private GImage textbox = new GImage("assets/ui/text_box.png");
+    public GImage textbox = new GImage("assets/ui/text_box.png");
+    public GImage character;
+    public GLabel name;
+    public GLabel dialogue;
     
     public void drawtextbox (GCanvas canvas){        
         textbox.setSize(canvas.getWidth()-10, canvas.getHeight()/4);
         double x = (canvas.getWidth()-(canvas.getWidth()-5));
-        double y = (canvas.getHeight()-(canvas.getHeight()/3));
+        double y = (canvas.getHeight()-(canvas.getHeight()/4));
         canvas.add(textbox, x, y);
     }
     
-    public void writetext (GCanvas canvas, String charname, String string){
-        double h =textbox.getHeight();
-        double w =textbox.getWidth();
+    public void writecharname(GCanvas canvas, String line){        
+        if(name!=null){
+            canvas.remove(name);
+        }   
+        int nameEnd2 = line.indexOf("_");
+        String charaname = line.substring(0, nameEnd2);
         double x =textbox.getX()+30;
-        double y =textbox.getY()+30;
-        double x2 =textbox.getX()+50;
-        double y2 =textbox.getY()+50;
-        GLabel name = new GLabel(charname);
-        name.setFont("Calibri-18-Bold");
+        double y =textbox.getY()+35;
+        name = new GLabel(charaname);
+        name.setFont("Dialog-25");
         name.setColor(Color.white);
         canvas.add(name, x, y);
-        GLabel text = new GLabel(string);
-        text.setFont("Calibri-16");
-        text.setColor(Color.white);
-        canvas.add(text, x, y2);
+        if(line.contains(":")){
+            writetext (canvas, line);
+        }
     }
     
+    
+    public void writetext (GCanvas canvas, String line){
+        if(line.contains(":")){
+            int textstart = line.indexOf(":")+1;
+            int textend = line.indexOf("]")-1;
+            String linetext = line.substring(textstart, textend);
+            double x =textbox.getX()+30;
+            double y2 =textbox.getY()+53;  
+            if(dialogue!=null){
+                canvas.remove(dialogue);  
+            }       
+            dialogue = new GLabel(linetext);
+            dialogue.setFont("Dialog-20");
+            dialogue.setColor(Color.white);
+            canvas.add(dialogue, x, y2);
+        }        
+    }
+    
+    public void addchar(GCanvas canvas, String line){    
+        //extracts name of character from string
+    //    int nameEnd2 = line.indexOf("_");
+    //    String charaname = line.substring(0, nameEnd2);
+    //    writecharname(canvas, charaname);
+        if(line.contains("_")){
+            writecharname(canvas, line);
+        }
+        //extracts image name from string
+        int nameEnd = line.indexOf(" ");
+        String filename = line.substring(0, nameEnd);
+        filename=("assets/char/" +filename);        
+        character= new GImage(filename);
+        character.setSize( character.getWidth()/1.5, character.getHeight()/1.5);
+        
+        
+        //sets position of character leftscreen, midscreen, rightscreen
+        if(line.contains("leftscreen")){
+            canvas.add(character, -175, 0);
+        }
+        else if (line.contains("midscreen")){
+            canvas.add(character, 0, 0);
+        }
+        else if (line.contains("rightscreen")){
+            canvas.add(character, 175, 0);
+        }
+        
+//        if(line.contains(":")){
+//            writetext (canvas, line);
+//       }
+        
+        character.sendBackward();
+        
+        if(textbox!=null){
+            textbox.sendToFront();
+        }       
+        if(name!=null){
+            name.sendToFront();            
+        }       
+        if(dialogue!=null){
+            dialogue.sendToFront();        
+        }        
+    }
 }
