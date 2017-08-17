0f2fec1851d389e5f1803a7389d135b4651591da
Gershom Narag <RGM-96X@LEGIO-II-AVGVSTA>
2017-08-17 22:25:00 +0800

    testing scene select

--- project/src/game/main/SelectScreen.java
+++ project/src/game/main/SelectScreen.java
@@ -1,5 +1,16 @@
 package game.main;
 
+import game.chapters.Chapter1;
+import game.main.MainProgram;
+import acm.graphics.*;
+
+
 public class SelectScreen {
+    
+    //GameCanvas canvas
+    
+    public void chap1house(GameCanvas canvas){
+       canvas.startchapter(21);
+}
     
 }
